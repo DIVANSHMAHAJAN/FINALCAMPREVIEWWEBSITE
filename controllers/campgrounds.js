bc47a772a8da9da051841a13c27220ffe5b1f3de
DIVANSHMAHAJAN <82203713+DIVANSHMAHAJAN@users.noreply.github.com>
2021-07-02 15:39:14 +0530

    console log added one line

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -58,6 +58,7 @@ module.exports.updateCampground = async (req, res) => {
     console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    console.log("kkkk");
     campground.images.push(...imgs);
     await campground.save();
     if (req.body.deleteImages) {
